b75173384857bcb54fcf7ad46d0f16a5cd2bf374
carol savchick <carolmariehinds@gmail.com>
2024-05-12 16:14:37 -0400

    turned in assignment, first attempt

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,19 @@
-# add your code here
+import pandas as pd
+
+fruits = pd.DataFrame(columns=['Apples', 'Bananas'])
+
+fruits_data = {
+        "Apples" :[35],
+        "Bananas" :[21]
+}
+
+pd.DataFrame(fruits_data)
+
+fruit_sales = pd.DataFrame({
+    "Apples": [35, 41],
+    "Bananas": [21,34]},
+    index = ["2017 Sales", "2018 Sales"])
+
+print(fruit_sales)
+
+fruit_sales.to_csv("fruit.csv", index=True)
